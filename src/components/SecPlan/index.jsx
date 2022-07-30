87a7af3b438e96e146f44b56bc6523eff399bad9
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-07-30 20:52:33 -0300

    ci: Cria seção de planos. A terminar.

---
+++ src/components/SecPlan/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import { FiGift } from 'react-icons/fi';
+
+function SecPlan({ items, modalities }) {
+  return (
+    <div className="border border-dark rounded rounded-2 d-flex">
+      <div>teste</div>
+      <div>teste</div>
+    </div>
+  );
+}
+
+SecPlan.propTypes = {
+  items: PropTypes.array
+};
+
+export default SecPlan;
