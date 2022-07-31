8d792c89b1b20860e139472c15c6febef38993cc
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-07-31 07:07:21 -0300

    ci: Atualiza seção planos com carousel.
    Mas ele ainda não aceita os cards de preço.

--- src/components/SecPlan/index.jsx
+++ src/components/SecPlan/index.jsx
@@ -8,10 +8,10 @@ function SecPlan({ items, modalities }) {
   return (
     // <div className="row">
     <div className="border border-dark rounded rounded-2 d-flex">
-      <div className="d-flex flex-column align-items-center text-center">
+      <div className="w-50 d-flex flex-column align-items-center text-center">
         <h3 className="mt-1">Nossos Planos</h3>
-        <div>
-          <img className="h-75" src={image} alt="wine image" />
+        <div className="h-75">
+          <img className="img-fluid" src={image} alt="wine image" />
         </div>
         <div className="mb-1">
           <Button>Saiba mais sobre o Clube Wine</Button>
